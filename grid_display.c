4732faf0a2af685a79106b3349a77c7dca600a47
Angela Mashuryan <m.angela.0118@gmail.com>
2025-01-08 20:02:13 -0500

    members

--- grid_display.c
+++ grid_display.c
@@ -23,7 +23,13 @@ int main(){
     int server_fd, client_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-    
-
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(server_fd == 0){
+        perror("socket failed");
+        exit(0); 
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
 
 }

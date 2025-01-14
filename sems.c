81288d0d63c9a13f7da2d77881cdef838def1171
Angela Mashuryan <m.angela.0118@gmail.com>
2025-01-14 15:29:51 -0500

    new file semaphore handling

---
+++ sems.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+
+#include <sys/ipc.h>

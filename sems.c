365805866ce21982acea34def8ccc6391c4d87ee
Angela Mashuryan <m.angela.0118@gmail.com>
2025-01-15 15:01:30 -0500

    unlock

--- sems.c
+++ sems.c
@@ -14,4 +14,18 @@ void lock(int semid){
     sb.sem_num = 0;
     sb.sem_op = -1;
     sb.sem_flg = 0;
+    semop(semid, &sb, 1);
+}
+
+void unlock(int semid){
+    struct sembuf sb;
+    sb.sem_num = 0;
+    sb.sem_op = 1;
+    sb.sem_flg = 0;
+    semop(semid,&sb,1);
+}
+
+int main(){
+    int semid = semget(//KEY, 1, 0666? | IPC_CREAT);
+    semctl(semid, 0 , )
 }

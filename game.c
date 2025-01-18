6eb84826826c1ec26a9c82ce1b16aee6e8e97a92
Angela Mashuryan <m.angela.0118@gmail.com>
2025-01-17 22:24:11 -0500

    control added for movements and navigation among grid

--- game.c
+++ game.c
@@ -77,21 +77,67 @@ void handle_signals(int signal){
     }
 }
 int main(){
-    
+    signal(SIGUSR1, handle_signals);
+    signal(SIGUSR2, handle_signals);
+
     int shmid;
-    shmid = shmget (12412, sizeof(Grid), 0666 | IPC_CREAT); //0600?
+    shmid = shmget (12412, sizeof(Grid), 0666 | IPC_CREAT); 
     if(shmid <0){
         perror("shmget fail");
-        exit(0);
+        exit(1);
     }
     Grid *grid = (Grid *) shmat(shmid, NULL, 0);
     if(grid == (void *)-1){
         perror("shmat fail");
-        exit(0);
+        exit(1);
     }
     initialize_grid(grid);
-    printf("grid initialized.");
+    display_grid(grid);
 
+    while(!grid->game_over){
+        char command;
+        printf("Enter command (w/a/s/d to move, q to quit): ");
+        scanf(" %c", &command);
+
+        if(command == 'q'){
+            grid->game_over = 1;
+            printf("Game over! You quit the game.\n");
+            break;
+        }
+
+        if(command == 'w' && grid->player_position >=5){
+            grid->player_position -=5;
+        }
+        else if(command == 'a' && grid->player_position %5 !=0){
+            grid->player_position -= 1;
+        }
+        else if(command == 's' && grid->player_position < GRID_SIZE -5){
+            grid->player_position += 5;
+        }
+        else if(command == 'd' && (grid->player_position +1) %5 != 0){
+            grid->player_position +=1;
+        }
+
+        char current_room = grid->grid[grid->player_position];
+        if(current_room == 'T'){
+            kill(getpid(), SIGUSR2); //trap
+            grid->game_over = 1;
+        }
+        else if(current_room == '$'){
+            grid->treasure_count++;
+            kill(getpid(), SIGUSR1);
+            grid->grid[grid->player_position] = ' ';
+        }
+        else if(current_room == 'M'){
+            printf("You encountered a monster! Game over!\n");
+            grid->game_over = 1;
+        }
+
+    
+
+    display_grid(grid);
+    save_grid(grid, "game_save.dat");
+}
     shmdt(grid);
     return(0);
 
